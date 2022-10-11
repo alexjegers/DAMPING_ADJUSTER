223b03f073531221a8e69a15a0c28e712afd5a8e
Alex Jegers <jeger007@d.umn.edu>
2022-10-11 12:59:13 -0500

    Shorted hex key

---
+++ Controller/Software/RemoteControlExe001/RemoteControlExe001/inc/iic.h
@@ -0,0 +1,16 @@
+/*
+ * iic.h
+ *
+ * Created: 10/11/2022 12:12:25 PM
+ *  Author: AJ992
+ */ 
+
+
+#ifndef IIC_H_
+#define IIC_H_
+
+
+
+
+
+#endif /* IIC_H_ */
---
+++ Controller/Software/RemoteControlExe001/RemoteControlExe001/scr/iic.cpp
@@ -0,0 +1,6 @@
+/*
+ * iic.cpp
+ *
+ * Created: 10/11/2022 12:12:35 PM
+ *  Author: AJ992
+ */ 
